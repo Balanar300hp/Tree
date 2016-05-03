6d1211d0e920c2fff2efbed0ad2525b7e5130414
Balanar300hp <kudashov.vlad@yandex.ru>
2016-05-03 18:44:47 +0300

    Update init.cpp

--- source/init.cpp
+++ source/init.cpp
@@ -1,210 +1,197 @@
-#include <binarysearchtree.cpp>
+#include <tree.cpp>
 #include <catch.hpp>
 #include <fstream>
 #include <iostream>
 using namespace std;
 
-SCENARIO("Add_int", "[add]"){
-  BinarySearchTree<int> tree;
-  REQUIRE(tree.add(7));
-  REQUIRE(tree.add(3));
-  REQUIRE(tree.add(5));
-  REQUIRE(tree.search(3));
-  REQUIRE(tree.search(5));
-  REQUIRE(tree.search(7));
+SCENARIO("Insert_int", "[add]"){
+  Tree<int> tree;
+  REQUIRE(tree.Insert(7));
+  REQUIRE(tree.Insert(3));
+  REQUIRE(tree.Insert(5));
+  REQUIRE(tree.Search(3));
+  REQUIRE(tree.Search(5));
+  REQUIRE(tree.Search(7));
 }
 
-SCENARIO("Add_char", "[add_c]"){
-  BinarySearchTree<char> tree;
-  REQUIRE(tree.add(5));
-  REQUIRE(tree.add(4));
-  REQUIRE(tree.add(6));
-  REQUIRE(tree.search(4));
-  REQUIRE(tree.search(5));
-  REQUIRE(tree.search(6));
+SCENARIO("Insert_char", "[add_c]"){
+  Tree<char> tree;
+  REQUIRE(tree.Insert(5));
+  REQUIRE(tree.Insert(4));
+  REQUIRE(tree.Insert(6));
+  REQUIRE(tree.Search(4));
+  REQUIRE(tree.Search(5));
+  REQUIRE(tree.Search(6));
 }
 
-SCENARIO("Add_double", "[add_d]"){
-  BinarySearchTree<double> tree;
-  REQUIRE(tree.add(7.62));
-  REQUIRE(tree.add(3.14));
-  REQUIRE(tree.add(5.85));
-  REQUIRE(tree.search(3.14));
-  REQUIRE(tree.search(5.85));
-  REQUIRE(tree.search(7.62));
+SCENARIO("Insert_double", "[add_d]"){
+  Tree<double> tree;
+  REQUIRE(tree.Insert(7.62));
+  REQUIRE(tree.Insert(3.14));
+  REQUIRE(tree.Insert(5.85));
+  REQUIRE(tree.Search(3.14));
+  REQUIRE(tree.Search(5.85));
+  REQUIRE(tree.Search(7.62));
 }
 
 SCENARIO("Search_int", "[search_i]") {
-BinarySearchTree<int> tree;
-tree.add(7);
-tree.add(5);
-tree.add(1);
-tree.add(9);
-tree.add(3);
-REQUIRE(tree.search(7));
-REQUIRE(tree.search(9));
-REQUIRE(tree.search(5));
-REQUIRE(tree.search(1));
-REQUIRE(tree.search(3));
-REQUIRE(!tree.search(0));
-REQUIRE(!tree.search(8));
-REQUIRE(!tree.search(6));
-REQUIRE(!tree.search(4));
-REQUIRE(!tree.search(2));
+Tree<int> tree;
+tree.Insert(7);
+tree.Insert(5);
+tree.Insert(1);
+tree.Insert(9);
+tree.Insert(3);
+REQUIRE(tree.Search(7));
+REQUIRE(tree.Search(9));
+REQUIRE(tree.Search(5));
+REQUIRE(tree.Search(1));
+REQUIRE(tree.Search(3));
+REQUIRE(!tree.Search(0));
+REQUIRE(!tree.Search(8));
+REQUIRE(!tree.Search(6));
+REQUIRE(!tree.Search(4));
+REQUIRE(!tree.Search(2));
 }
 
 SCENARIO("Search_char", "[search_c]") {
 BinarySearchTree<char> tree;
-tree.add(7);
-tree.add(5);
-tree.add(1);
-tree.add(9);
-tree.add(3);
-REQUIRE(tree.search(7));
-REQUIRE(tree.search(9));
-REQUIRE(tree.search(5));
-REQUIRE(tree.search(1));
-REQUIRE(tree.search(3));
-REQUIRE(!tree.search(0));
-REQUIRE(!tree.search(8));
-REQUIRE(!tree.search(6));
-REQUIRE(!tree.search(4));
-REQUIRE(!tree.search(2));
+tree.Insert(7);
+tree.Insert(5);
+tree.Insert(1);
+tree.Insert(9);
+tree.Insert(3);
+REQUIRE(tree.Search(7));
+REQUIRE(tree.Search(9));
+REQUIRE(tree.Search(5));
+REQUIRE(tree.Search(1));
+REQUIRE(tree.Search(3));
+REQUIRE(!tree.Search(0));
+REQUIRE(!tree.Search(8));
+REQUIRE(!tree.Search(6));
+REQUIRE(!tree.Search(4));
+REQUIRE(!tree.Search(2));
 }
 
 SCENARIO("Search_double", "[search_d]") {
-BinarySearchTree<double> tree;
-tree.add(7.77);
-tree.add(5.85);
-tree.add(1.29);
-tree.add(9.999);
-tree.add(3.14);
-REQUIRE(tree.search(7.77));
-REQUIRE(tree.search(9.999));
-REQUIRE(tree.search(5.85));
-REQUIRE(tree.search(1.29));
-REQUIRE(tree.search(3.14));
-REQUIRE(!tree.search(0.5));
-REQUIRE(!tree.search(8.800));
-REQUIRE(!tree.search(6.666));
-REQUIRE(!tree.search(4.13));
-REQUIRE(!tree.search(2.34));
+Tree<double> tree;
+tree.Insert(7.77);
+tree.Insert(5.85);
+tree.Insert(1.29);
+tree.Insert(9.999);
+tree.Insert(3.14);
+REQUIRE(tree.Search(7.77));
+REQUIRE(tree.Search(9.999));
+REQUIRE(tree.Search(5.85));
+REQUIRE(tree.Search(1.29));
+REQUIRE(tree.Search(3.14));
+REQUIRE(!tree.Search(0.5));
+REQUIRE(!tree.Search(8.800));
+REQUIRE(!tree.Search(6.666));
+REQUIRE(!tree.Search(4.13));
+REQUIRE(!tree.Search(2.34));
 }
 
 SCENARIO("Read_int","[read_i]"){
-BinarySearchTree<int> tree; ifstream fin("read_file_test.txt");
+Tree<int> tree; ifstream fin("read.txt");
 REQUIRE(fin>>tree);
 fin.close();
-REQUIRE(tree.search(7));
-REQUIRE(tree.search(9));
-REQUIRE(tree.search(5));
-REQUIRE(tree.search(1));
-REQUIRE(tree.search(3));
+REQUIRE(tree.Search(7));
+REQUIRE(tree.Search(9));
+REQUIRE(tree.Search(5));
+REQUIRE(tree.Search(1));
+REQUIRE(tree.Search(3));
 }
 
 SCENARIO("Read_double","[read_d]"){
-BinarySearchTree<double> tree; ifstream fin("read_file_test_double.txt");
+Tree<double> tree; ifstream fin("read_db.txt");
 REQUIRE(fin>>tree);
 fin.close();
-REQUIRE(tree.search(12.74));
-REQUIRE(tree.search(15.62));
-REQUIRE(tree.search(7.62));
-REQUIRE(tree.search(3.14));
-REQUIRE(tree.search(8.34));
-REQUIRE(tree.search(25.15));
-REQUIRE(tree.search(17.16));
+REQUIRE(tree.Search(12.74));
+REQUIRE(tree.Search(15.62));
+REQUIRE(tree.Search(7.62));
+REQUIRE(tree.Search(3.14));
+REQUIRE(tree.Search(8.34));
+REQUIRE(tree.Search(25.15));
+REQUIRE(tree.Search(17.16));
 }
 
 SCENARIO("Print_file_int","[print_file_i]"){
-BinarySearchTree<int> tree, tree2; ofstream fout("print_file_test.txt", ios::app);
-  tree.add(7);
-  tree.add(3);
-  tree.add(5);
+Tree<int> tree; ofstream fout("print.txt", ios::app);
+  tree.Insert(7);
+  tree.Insert(3);
+  tree.Insert(5);
   REQUIRE(fout<<tree);
-/*  ifstream fin("print_file_test.txt");
-  fin>>tree_2;
-  REQUIRE(tree_2.search(7));
-  REQUIRE(tree_2.search(3));
-  REQUIRE(tree_2.search(5));*/
 }
 
 SCENARIO("Print_file_char","[print_file_c]"){
-BinarySearchTree<char> tree, tree2; ofstream fout("print_file_test_char.txt", ios::app);
-  tree.add(7);
-  tree.add(3);
-  tree.add(5);
+Tree<char> tree; ofstream fout("print_chr.txt", ios::app);
+  tree.Insert(7);
+  tree.Insert(3);
+  tree.Insert(5);
   REQUIRE(fout<<tree);
- /* ifstream fin("print_file_test_char.txt");
-  fin>>tree_2;
-  REQUIRE(tree_2.search(7));
-  REQUIRE(tree_2.search(3));
-  REQUIRE(tree_2.search(5));*/
+
 }
 
 SCENARIO("Print_file_double","[print_file_d]"){
-BinarySearchTree<double> tree, tree2; ofstream fout("print_file_test_double.txt", ios::app);
-  tree.add(7.77);
-  tree.add(3.33);
-  tree.add(5.55);
+Tree<double> tree; ofstream fout("print_db.txt", ios::app);
+  tree.Insert(7.77);
+  tree.Insert(3.33);
+  tree.Insert(5.55);
   REQUIRE(fout<<tree);
-/*  ifstream fin("print_file_test_double.txt");
-  fin>>tree_2;
-  REQUIRE(tree_2.search(7.77));
-  REQUIRE(tree_2.search(3.33));
-  REQUIRE(tree_2.search(5.55));*/
+
 }
 
 
 SCENARIO("Print_console_int", "[print_console_i]"){
-BinarySearchTree<int> tree;  
-  tree.add(7);
-  tree.add(3);
-  tree.add(5);
+Tree<int> tree;  
+  tree.Insert(7);
+  tree.Insert(3);
+  tree.Insert(5);
   REQUIRE(cout<<tree);
 }
 
 SCENARIO("Print_console_char", "[print_console_c]"){
-BinarySearchTree<char> tree;  
-  tree.add(7);
-  tree.add(3);
-  tree.add(5);
+Tree<char> tree;  
+  tree.Insert(7);
+  tree.Insert(3);
+  tree.Insert(5);
   REQUIRE(cout<<tree);
 }
 
 SCENARIO("Print_console_double", "[print_console_d]"){
-BinarySearchTree<double> tree;  
-  tree.add(7);
-  tree.add(3);
-  tree.add(5);
+Tree<double> tree;  
+  tree.Insert(7);
+  tree.Insert(3);
+  tree.Insert(5);
   REQUIRE(cout<<tree);
 }
 
 SCENARIO("Iscl_add", "[I_a]"){
-BinarySearchTree<int> tree; int O=0; 
-tree.add(1);
-try{tree.add(1);}
-catch(Uzhe_est &e){O++;}
+Tree<int> tree; int O=0; 
+tree.Insert(1);
+try{tree.Insert(1);}
+catch(Exist &e){O++;}
 REQUIRE(O==1);
 }
 
 SCENARIO("Iscl_not_open", "[I_no]"){
-BinarySearchTree<int> tree; int O=0; ifstream fin("AVADA_KEDAVRA");
+Tree<int> tree; int O=0; ifstream fin("errortype");
 try{fin>>tree;}
 catch(File_Not_Open &e){O++;}
 REQUIRE(O==1);
 }
 
 SCENARIO("Iscl_pust1", "[I_p1]"){
-BinarySearchTree<int> tree; int O=0; 
+Tree<int> tree; int O=0; 
 try{cout<<tree;}
-catch(Pustoe_derevo &e){O++;}
+catch(Empty &e){O++;}
 REQUIRE(O==1);
 }
 
 SCENARIO("Iscl_pust2", "[I_p2]"){
-BinarySearchTree<int> tree; int O=0; ofstream fout("print_file_test_double.txt", ios::app);
+Tree<int> tree; int O=0; ofstream fout("print_db.txt", ios::app);
 try{fout<<tree;}
-catch(Pustoe_derevo &e){O++;}
+catch(Empty &e){O++;}
 fout.close();
 REQUIRE(O==1);
 }
